36b28590349e25ede16db4dcdc9c087d3ed8bca2
Ishaan Gaba <ishaangaba97@gmail.com>
2024-01-25 10:19:36 +0530

    first commit

---
+++ reciever1.js
@@ -0,0 +1,23 @@
+const amqp = require('amqplib');
+
+const receiveMessage = async () => {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  const exchangeName = 'exampleExchange';
+
+  await channel.assertExchange(exchangeName, 'fanout', { durable: false });
+
+  const { queue } = await channel.assertQueue('', { exclusive: true });
+  channel.bindQueue(queue, exchangeName, '');
+
+  console.log(`[Receiver1] Waiting for messages. To exit press CTRL+C`);
+
+  channel.consume(queue, (msg) => {
+    if (msg.content) {
+      const message = JSON.parse(msg.content.toString());
+      console.log(`[Receiver1] Received message: ${JSON.stringify(message)}`);
+    }
+  }, { noAck: true });
+}
+
+receiveMessage();
---
+++ sender.js
@@ -0,0 +1,21 @@
+const amqp = require('amqplib');
+
+const sendMessage = async (obj) => {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  const exchangeName = 'exampleExchange';
+
+  await channel.assertExchange(exchangeName, 'fanout', { durable: false });
+
+  const message = obj;
+  channel.publish(exchangeName, '', Buffer.from(JSON.stringify(message)));
+
+  console.log(`[Sender] Sent message: ${JSON.stringify(message)}`);
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 1000);
+}
+
+sendMessage({ name: 'Ishaan', age: '21' });
